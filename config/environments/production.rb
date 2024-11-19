07760cdaa1190c9ba56adf3a6fc706ea211fea87
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-11-19 12:31:50 +0700

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.active_record.sqlite3_production_warning=false
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
